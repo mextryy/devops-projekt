30de12b819d935631da4c6b8b95c4c3b9b9a456f
mextryy <atryndoch@student.agh.edu.pl>
2026-06-11 18:18:05 +0200

    feat: ostateczna wersja plików

---
+++ app.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/health':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello World")
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
+    print("Server started on port 8080...")
+    httpd.serve_forever()
